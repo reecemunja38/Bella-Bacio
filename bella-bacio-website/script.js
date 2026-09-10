e06c207d076b892f0940e7d112643eae08f3aad2
reecemunja38 <reecemunja38@gmail.com>
2026-09-09 23:55:37 -0400

    Delete bella-bacio-website directory

--- bella-bacio-website/script.js
+++
@@ -1,5 +0,0 @@
-document.getElementById("year").textContent = new Date().getFullYear();
-
-function showMessage() {
-  alert("Your shop button is ready! Next we can connect this product to your checkout or order page.");
-}
