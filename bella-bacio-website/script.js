e4d25ea0630d430ef80bbfbd9600edb264245aeb
reecemunja38 <reecemunja38@gmail.com>
2026-09-09 23:30:59 -0400

    Add files via upload

---
+++ bella-bacio-website/script.js
@@ -0,0 +1,5 @@
+document.getElementById("year").textContent = new Date().getFullYear();
+
+function showMessage() {
+  alert("Your shop button is ready! Next we can connect this product to your checkout or order page.");
+}
